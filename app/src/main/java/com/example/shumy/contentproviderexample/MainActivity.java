e345f9a814f186e671489102d9fedc14e9ff149f
WillemSchoeman <willem.schoeman17@gmail.com>
2018-07-05 16:49:25 +0200

    Permission changes.

--- app/src/main/java/com/example/shumy/contentproviderexample/MainActivity.java
+++ app/src/main/java/com/example/shumy/contentproviderexample/MainActivity.java
@@ -1,24 +1,32 @@
 package com.example.shumy.contentproviderexample;
 
+import android.Manifest;
 import android.content.ContentResolver;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
     private ListView contactNames;
+    private static final int REQUEST_CODE_READ_CONTACTS = 1;
+    private static boolean READ_CONTACTS_GRANTED = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +37,9 @@ public class MainActivity extends AppCompatActivity {
 
         contactNames = findViewById(R.id.contact_names);
 
+        int hasReadContactPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS);
+        Log.d(TAG, "onCreate: checkSelfPermission = " + hasReadContactPermission);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,12 +47,25 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(TAG, "onClick: starts");
                 String[] projection = {ContactsContract.Contacts.DISPLAY_NAME_PRIMARY};
                 ContentResolver contentResolver = getContentResolver();
-                Cursor cursor = contentResolver.query(ContactsContract.Contacts.CONTENT_URI,projection,
+                Cursor cursor = contentResolver.query(ContactsContract.Contacts.CONTENT_URI,
+                        projection,
                         null,
                         null,
-                        ContactsContract.Contacts.DISPLAY_NAME);
+                        ContactsContract.Contacts.DISPLAY_NAME_PRIMARY);
+
+                if(cursor != null) {
+                    List<String> contacts = new ArrayList<String>();
+                    while (cursor.moveToNext()) {
+                        contacts.add(cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME_PRIMARY)));
+                    }
+                    cursor.close();
+                    ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this,R.layout.contact_detail,R.id.name_textview,contacts);
+                    contactNames.setAdapter(adapter);
+                }
+                Log.d(TAG, "fab onClick: ends");
             }
         });
+        Log.d(TAG, "onCreate: ends");
     }
 
     @Override
